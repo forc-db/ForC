4b2a70c3fcefed0eea3d48bce9ea8cbf70cfc6c6
ValentineHerr <herrmannv@si.edu>
2017-08-24 12:24:18 -0400

    Load scripts and figures
    Also creating a new folder in suplementary resoures to create data used to plot Wold Map.
    issue #39

---
+++ scripts/World_Map_with_Biogeographic_Regions_and_ForC_Sites.R
@@ -0,0 +1,85 @@
+######################################################
+# Purpose: Plot a World map with Biogeographic regions and ForC sites, with or without Biogeographic zones and chart.
+# Inputs: - pre-saved R environment (data_for_World_Map_with_Biogeographic_regions_and_sites.Rdata), created by script. Both script and .Rdata file are in supplementary_resources/World Map data
+# outputs: 2 .png files with figure of World Map with our without Biogegraphic zones and chart.
+# Developped by: Valentine Herrmann - HerrmannV@si.edu
+# R version 3.4.0 (2017-08-23)
+######################################################
+
+
+# Clean environment ####
+rm(list = ls())
+
+# Setup working directory ####
+setwd(".")
+
+# Load libraries ####
+library(rgdal)
+
+# Load pre-saved R environment ####
+load("supplementary_resources/World Map data/data_for_World_Map_with_Biogeographic_Regions_and_ForC_Sites.Rdata")
+
+# Plot ####
+
+## With biogeographic regions and chart####
+
+png("figures/World_Map_with_Biogeographic_regions_and_sites.png", width=169, height = 100, units = "mm", res = 300, pointsize = 8)
+par(mar = c(0,0,0,0))
+
+plot(SYNMAP, col = "grey",  border = "transparent")
+plot(ECOREGIONS, add = T, col = ECOREGIONS_colors, border = "white")
+plot(SYNMAP, add = T, col = SYNMAP_density, border = "transparent")
+
+
+points(SITES, bg = SITES$Color, pch = 21, col = "black", lwd = 0.5)
+
+rect(xleft = -180, xright = 180, ybottom = -91, ytop = -60, col = "white", border = "transparent")
+# legend
+
+
+legend(-182, -40, pch = 21, pt.bg = rbPal(6), legend = c("1", "2-10", "11-20", "21-40", "41-80", "81-277"), bty = "n", title = expression(bold("No. of records")))
+
+legend(-130, -50, fill = rgb(matrix(0, nrow = 4, ncol = 3), alpha = c(255, 150, 100, 50), maxColorValue = 255), border = "transparent", legend = c("Evegreen", "Decidous", "MixedED", "Other"), bty = "n", title = expression(bold("Forest Cover")))
+
+legend(-60, -50, fill = rgb(t(col2rgb(c("goldenrod3", "violetred3", "cadetblue4", "chocolate", "darkgreen", "darkorchid4", "darkred"))), alpha = rep(200, 8), maxColorValue = 255), border = "transparent", legend = names(ECOREGIONS)[-2], bty = "n", title = expression(bold("Biogeographic zone")), ncol = 2)
+
+par()$fig
+par(fig = c(0.63, 1, 0, 0.41), new = T)
+par(mar = c(5.1, 4.1, 4.1, 2.1))
+par(oma = c(0,5,0,0))
+
+b <- barplot(t(rbind(forested_area/sum(forested_area), No._of_sites/sum(No._of_sites), No._of_plots/sum(No._of_plots), No._of_records/sum(No._of_records))), col = rgb(t(col2rgb(c("goldenrod3", "grey", "violetred3", "cadetblue4", "chocolate", "darkgreen", "darkorchid4", "darkred")[order(names(ECOREGIONS))][-2])), alpha = rep(200, 8), maxColorValue = 255), horiz = T, xaxt = "n", border = "transparent")
+
+
+axis(1, at = c(0, .5, 1), labels = c("0%", "50%"," 100%"))
+mtext(c("No. of records", "No. of plots", "No. of sites", "forested area"), side = 2, at = rev(b), las = 1, line = 1)
+
+dev.off()
+
+
+## Without biogeographic regions and chart (Just colors for forest cover)####
+
+png("figures/World_Map_of_sites.png", width=169, height = 100, units = "mm", res = 300, pointsize = 8)
+par(mar = c(0,0,0,0))
+
+
+
+names(SYNMAP)
+SYNMAP_forest_color <- c("darkolivegreen3", "darkolivegreen", "darkolivegreen4", "gainsboro")
+
+
+plot(SYNMAP, col = SYNMAP_forest_color,  border = "transparent")
+axis(2)
+box()
+points(SITES, bg = SITES$Color, pch = 21, col = "black", lwd = 0.5)
+
+rect(xleft = -180, xright = 180, ybottom = -91, ytop = -60, col = "white", border = "transparent")
+# legend
+
+
+legend(-182, 0, pch = 21, pt.bg = rbPal(6), legend = c("1", "2-10", "11-20", "21-40", "41-80", "81-277"), bty = "n", title = expression(bold("No. of records")))
+
+legend(-130, -10, fill =SYNMAP_forest_color[c(2,3,1,4)], border = "transparent", legend = c("Evegreen", "MixedED", "Decidous", "Other"), bty = "n", title = expression(bold("Forest Cover")))
+
+dev.off()
+
---
+++ supplementary_resources/World Map data/Create_data_for_World_Map_with_Biogeographic_regions_and_sites.R
@@ -0,0 +1,131 @@
+######################################################
+# Purpose: Prepare data used by the script that plots a World map with Biogeographic regions and ForC sites.
+# Inputs: - SYNMAP shapefile (polygons, dissolved to b smaller) - You can download SYNMAP maps here: ftp://nacp.ornl.gov/synthesis/2009/frescati/model_driver/land_use_change/analysis/global/Hurtt_SYNMAP/ 
+#         - SYNMAP legend file
+#         - WWF ECOREGIONS shapefile (polygons of ecoregions) - You can download this map here: https://www.sciencebase.gov/catalog/item/508fece8e4b0a1b43c29ca22
+#         - ForC SITES table
+#         - ForC MEASUREMENTS table
+#         - ForC HISTORY table
+# outputs: .Rdata file with all objects needed to create map
+# Developped by: Valentine Herrmann - HerrmannV@si.edu
+# R version 3.4.0 (2017-08-24)
+######################################################
+
+
+# Clean environment ####
+rm(list = ls())
+
+# Setup working directory ####
+setwd(".")
+
+# Load libraries ####
+library(rgdal)
+library(rgeos)
+
+
+# LOAD SYNMAP MAP AND LEGEND ####
+
+
+SYNMAP <- readOGR("R:/Global Maps Data/SYNMAP_Hurtt/synmap_polygon_dissolve.shp")
+
+SYNMAP$GRIDCODE <- as.numeric(as.character(SYNMAP$GRIDCODE))
+
+
+SYNMAP_legend <- read.csv("R:/Global Maps Data/SYNMAP_Hurtt/SYNMAP_Legend.csv")
+
+## Separate gricodes of synmap into forest_cover types
+
+Evegreen_GRIDCODE <- c(1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34)
+Deciduous_GRIDCODE <- c(2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35)
+MixedED_GRIDCODE <- c(3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36)
+
+SYNMAP$Forest_Cover <- ifelse(SYNMAP$GRIDCODE  %in% Evegreen_GRIDCODE, "Evergreen",
+                              ifelse(SYNMAP$GRIDCODE %in% Deciduous_GRIDCODE, "Deciduous",
+                                     ifelse(SYNMAP$GRIDCODE %in% MixedED_GRIDCODE, "MixedED", "Other")))
+
+## Disolve 
+SYNMAP <- gUnaryUnion(SYNMAP, id = SYNMAP$Forest_Cover)
+
+## Any self-intersection ??
+
+which(!gIsValid(SYNMAP, byid = TRUE))
+
+# LOAD WWF ECOREGIONS MAP ####
+
+ECOREGIONS <-  readOGR("R:/Global Maps Data/Shapefiles/WWF Terrestrial Ecoregions/modifications for TropForC fig/wwf_terr_ecos.shp")
+
+unique(ECOREGIONS$REALM_1)
+
+ECOREGIONS <- gUnaryUnion(ECOREGIONS, id = ECOREGIONS$REALM_1)
+summary(ECOREGIONS)
+
+## Any self-intersection ??
+
+which(!gIsValid(ECOREGIONS, byid = TRUE))
+
+
+# LOAD COUNTOUR MAP ####
+
+
+
+# CREATE COLOR VECTORS
+names(ECOREGIONS)
+ECOREGIONS_colors = c("goldenrod3", "grey", "violetred3", "cadetblue4", "chocolate", "darkgreen", "darkorchid4", "darkred")
+
+names(SYNMAP)
+SYNMAP_density <- rgb(c(255,255,255,255), c(255,255,255,255), c(255,255,255,255), alpha =  c(100,0,150,200), maxColorValue = 255)
+
+
+# ADD SITES
+
+MEASUREMENTS <- read.csv("data/ForC_measurements.csv", stringsAsFactors = F)
+SITES <- read.csv("data/ForC_sites.csv", stringsAsFactors = F)
+
+No.of.records <- tapply(MEASUREMENTS$mean, MEASUREMENTS$sites.sitename, function(x) sum(!is.na(x)))
+No.of.records <- data.frame(sites.sitename = rownames(No.of.records), No.of.records = as.vector(No.of.records))
+str(No.of.records)
+
+SITES <- SITES[, c("sites.sitename", "lat", "lon", "FAOecozone")]
+SITES <- merge(SITES, No.of.records, by = "sites.sitename")
+
+
+
+coordinates(SITES) <- c("lon", "lat")
+str(SITES)
+proj4string(SITES) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0 ")
+
+
+rbPal <- colorRampPalette(c("yellow", "red4"))
+SITES$Color <- rbPal(6)[as.numeric(cut(SITES$No.of.records, breaks = c(0,1,10,20,40,80,277)))]
+
+
+## make inset plot ####
+
+SITES$Ecoregion <- over(SITES,ECOREGIONS) 
+SITES$Ecoregion <- names(ECOREGIONS)[SITES$Ecoregion]
+
+HISTORY <- read.csv("data/ForC_history.csv", stringsAsFactors = F)
+
+
+HISTORY_Summary <- merge(HISTORY[, c("sites.sitename", "plot.name")], SITES[, c("sites.sitename", "Ecoregion")])
+MEASUREMENTS_Summary <- merge(MEASUREMENTS[, c("sites.sitename", "plot.name")], SITES[, c("sites.sitename", "Ecoregion")])
+
+
+### forested area
+forested_area <- table(SITES$Ecoregion[grepl("forest", SITES$FAOecozone)])
+
+### No. of sites
+No._of_sites <- table(SITES$Ecoregion)
+
+### No. of plots
+No._of_plots <- table(HISTORY_Summary$Ecoregion)
+
+### No. of records
+No._of_records <- table(MEASUREMENTS_Summary$Ecoregion)
+
+
+
+save.image("supplementary_resources/World Map data/data_for_World_Map_with_Biogeographic_Regions_and_ForC_Sites.Rdata")
+
+
+
