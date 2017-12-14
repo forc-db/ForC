7ca6d50881921d3d8574aaa9bf201aea302f2306
Valentine Herrmann <herrmannv@si.edu>
2017-12-14 10:30:04 -0500

    Update figure to use end_date or date or citation year, in that order

--- scripts/Figures/Histogram_of_number_of_records_by_measurement_date.R
+++ scripts/Figures/Histogram_of_number_of_records_by_measurement_date.R
@@ -21,14 +21,15 @@ MEASUREMENTS <- read.csv("data/ForC_measurements.csv", stringsAsFactors = F)
 # Prepare data to plot ####
 
 
-dates.collection <- substr(MEASUREMENTS$end_date, 1,4)
-dates.collection <- as.numeric(ifelse(dates.collection %in% c("NI", "NRA", "NaN", "NAC") | is.na(dates.collection), NA, dates.collection))
+dates.collection <- as.numeric(substr(MEASUREMENTS$end_date, 1,4))
+dates.collection <- ifelse(is.na(dates.collection), as.numeric(substr(MEASUREMENTS$date, 1,4)), dates.collection)
 
-dates.citation <- ifelse(is.na(dates.collection), MEASUREMENTS$citations.year, NA)
+citations.DOI.to.exclude <- c("10.1111/j.1365-2486.2007.01420.x", "10.1111/j.1365-2486.2007.01439.x", "10.1071/BT07151", "10.1111/geb.12113", "10.1073/pnas.1317065111") # DOI corresponding to Litton et al. 2007, Luyssaert et al. 2007, Baldocchi 2008, Liu et al. 2014, Yu et al. 2014.
 
+dates.citation <- ifelse(is.na(dates.collection) & !MEASUREMENTS$citations.doi %in% citations.DOI.to.exclude, MEASUREMENTS$citations.year, NA) # Replace NA by citation year excluding publication dates of big compilation listed above.
 
 all.dates <- range(dates.collection, dates.citation, na.rm = T)
-all.dates <-all.dates[1]:all.dates[2]
+all.dates <- all.dates[1]:all.dates[2]
 
 
 dates.collection <- factor(dates.collection, levels = all.dates)
@@ -41,9 +42,18 @@ table.citation <- table(dates.citation)
 
 data.to.plot <- rbind(table.collection, table.citation)
 
-## make 5-year bins
-bins <- rep(1:(ncol(data.to.plot)/5), each = 5)
-bins.names <- paste(all.dates[c(TRUE, rep(FALSE, 4))], all.dates[c(rep(FALSE, 4), TRUE)], sep = " - ")
+## make x-year bins
+
+bin_size <- 5
+year0 <- ifelse(bin_size == 5, 1961, 1960)
+
+# bins <- c(rep(1:(ncol(data.to.plot) %/% bin_size), each = bin_size), rep(ncol(data.to.plot) %/% bin_size +1, ncol(data.to.plot) %% 5)) 
+
+bins <- c(rep(1, year0-1934), rep(2:c((2015 - year0+1) %/% bin_size +1) , each = bin_size), rep(c(2015- year0+1) %/% bin_size + 2, c(2015- year0+1)  %% bin_size))
+
+
+bins.names <- tapply(all.dates, bins, function(x) paste(range(x), collapse = " - "))
+bins.names[1] <- "pre-1960"
 
 data.to.plot <- t(apply(data.to.plot, 1, function(x) tapply(x, bins, sum)))
 colnames(data.to.plot) <- bins.names
