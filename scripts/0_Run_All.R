9afa657b219ea82087ff02b1c8775332fc8f2528
Valentine Herrmann <herrmannv@si.edu>
2018-12-31 15:46:14 -0500

    upload (still a work in progress though!)

---
+++ scripts/0_Run_All.R
@@ -0,0 +1,52 @@
+######################################################
+# Purpose: Run all necessary scripts after an update in one of the table of the database
+# Developped by: Valentine Herrmann - HerrmannV@si.edu
+#  R version 3.5.1 (2018-07-02)
+######################################################
+
+
+# Clean environment ####
+rm(list = ls())
+
+# Set working directory as ForC main folder ####
+setwd(".")
+
+
+# Run all necessary scripts
+
+## Generate plots from History ####
+source("scripts/Database_manipulation/Generate PLOTS from HISTORY/ForC_plots_from_history.R")
+
+## Group sites into areas ?
+# source("scripts/Database_manipulation/Group_sites_into_areas/Group_sites_into_geographic_areas_25km.R")
+
+## Generate ForC_simplified and ForC_simplified_metadata ####
+### this internalky runs (source("scripts/Database_manipulation/Reconcile_duplicated_records.R"))
+source("scripts/Database_manipulation/Create_ForC_simplified.R")
+source("scripts/Database_manipulation/Create_ForC_simplified_metadata.R")
+
+
+## Database numbers and facts ####
+source("scripts/Database_numbers_and_facts/Calculate_biome_average_for_any_ForC_variable.R")
+source("scripts/Database_numbers_and_facts/Sample_size_per_variable_and forest_biome_combination.R")
+source("scripts/Database_numbers_and_facts/Test_for_C_cycle_closure_(consistency).R")
+
+## Figures ####
+source("scripts/Figures/Age_trend_in_C_cycle_variables.R")
+source("scripts/Figures/C_Cylce_diagrams.R")
+source("scripts/Figures/Figure_of_Climate_of_forC_sites.R")
+source("scripts/Figures/Histogram_of_dominant_vegetation.R")
+source("scripts/Figures/Histogram_of_number_of_records_by_measurement_date.R")
+source("scripts/Figures/Histogram_of_sites_elevetion.R")
+source("scripts/Figures/Histogram_of_Stand_Age.R")
+source("scripts/Figures/World_Map_with_Biogeographic_Regions_and_ForC_Sites.R")
+
+
+
+
+
+
+
+
+
+
