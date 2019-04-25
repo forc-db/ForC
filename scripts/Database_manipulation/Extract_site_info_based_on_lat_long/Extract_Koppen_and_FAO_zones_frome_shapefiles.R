3bfbc78363e7729be4b3970e048c9410866bab63
Valentine Herrmann <herrmannv@si.edu>
2019-04-25 15:17:26 -0400

    update to deal with sites with missing coordinates

--- scripts/Database_manipulation/Extract_site_info_based_on_lat_long/Extract_Koppen_and_FAO_zones_frome_shapefiles.R
+++ scripts/Database_manipulation/Extract_site_info_based_on_lat_long/Extract_Koppen_and_FAO_zones_frome_shapefiles.R
@@ -35,8 +35,9 @@ continents <- readOGR("supplementary_resources/World Map data/Continents/World_C
 
 
 # set SITES as coordinates ####
+idx.non.missing.coordinates <- apply(SITES[, c("lon", "lat")], 1, function(x) !any(is.na(x)))
 
-SITES.xy <- SITES
+SITES.xy <- SITES[idx.non.missing.coordinates, ]
 coordinates(SITES.xy) <- SITES.xy[, c("lon", "lat")]
 proj4string(SITES.xy) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0")
                              
@@ -49,31 +50,41 @@ proj4string(SITES.xy) <- CRS("+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +
 
 new.FAO <- over(SITES.xy, FAO)$gez_name
 
-new.FAO[SITES$FAO.ecozone %in% "NAC"]
+new.FAO[SITES[idx.non.missing.coordinates, ]$FAO.ecozone %in% "NAC"]
 
 
-cbind(SITES$sites.sitename, SITES$FAO.ecozone, new.FAO)[!apply(cbind(SITES$FAO.ecozone, new.FAO), 1, function(x) x[1]%in% x[2]),] # double checking that only "NAC" are given a "different" FAO.ecozone
+View(cbind(SITES[idx.non.missing.coordinates, ]$sites.sitename, SITES[idx.non.missing.coordinates, ]$FAO.ecozone, new.FAO)[!apply(cbind(SITES[idx.non.missing.coordinates, ]$FAO.ecozone, new.FAO), 1, function(x) x[1]%in% x[2]),]) # double checking that only "NAC" are given a "different" FAO.ecozone
 
 # Ignore water and keep what was there before
-new.FAO[new.FAO %in% "Water"] <- SITES$FAO.ecozone[new.FAO %in% "Water"]
+new.FAO[new.FAO %in% "Water"] <- SITES[idx.non.missing.coordinates, ]$FAO.ecozone[new.FAO %in% "Water"]
 
-# Double check othere differences
-cbind(SITES$sites.sitename, SITES$FAO.ecozone, new.FAO)[!apply(cbind(SITES$FAO.ecozone, new.FAO), 1, function(x) x[1] %in% x[2]),]
+# Ignore NA and keep what was there before
+new.FAO[is.na(new.FAO)] <- SITES[idx.non.missing.coordinates, ]$FAO.ecozone[is.na(new.FAO)]
 
 
+# Double check other differences
+cbind(SITES[idx.non.missing.coordinates, ]$sites.sitename, SITES[idx.non.missing.coordinates, ]$FAO.ecozone, new.FAO)[!apply(cbind(SITES[idx.non.missing.coordinates, ]$FAO.ecozone, new.FAO), 1, function(x) x[1] %in% x[2]),]
+
+table( SITES[idx.non.missing.coordinates, ]$FAO.ecozone[!apply(cbind(SITES[idx.non.missing.coordinates, ]$FAO.ecozone, new.FAO), 1, function(x) x[1] %in% x[2])], new.FAO[!apply(cbind(SITES[idx.non.missing.coordinates, ]$FAO.ecozone, new.FAO), 1, function(x) x[1] %in% x[2])]) # if only one row for "NAC, you are good to go
 
 # Replace all FAO.ecozone by the ones extracted
 
-SITES$FAO.ecozone <- new.FAO
+SITES[idx.non.missing.coordinates, ]$FAO.ecozone <- new.FAO
 
-SITES[is.na(SITES$FAO.ecozone), c("sites.sitename", "lat", "lon", "country", "FAO.ecozone", "biogeog", "Koeppen")]
+SITES[is.na(SITES$FAO.ecozone), c("sites.sitename", "lat", "lon", "country", "FAO.ecozone", "biogeog", "Koeppen")] # should be empty
 
+SITES[SITES$FAO.ecozone %in% "NAC", c("sites.sitename", "lat", "lon", "country", "FAO.ecozone", "biogeog", "Koeppen")] # if there are still "NAC", and lat lon are not NA, that means the coordinates are falling in a place where no FAO are define... 
 
-SITES$FAO.ecozone[is.na(SITES$FAO.ecozone) & SITES$country %in% "Japan" & SITES$Koeppen %in% "Cfa"] <- "Subtropical humid forest"
-SITES$FAO.ecozone[is.na(SITES$FAO.ecozone) & SITES$country %in% "United States of America" & SITES$Koeppen %in% c("Cfa", "Dfb")] <- "Temperate continental forest"
-SITES$FAO.ecozone[is.na(SITES$FAO.ecozone) & SITES$country %in% c("Brazil", "French Guiana")  & SITES$Koeppen %in% "Af"] <- "Tropical rainforest"
+SITES[SITES$FAO.ecozone %in% "NAC" & !is.na(SITES$lat) , c("sites.sitename", "lat", "lon", "country", "FAO.ecozone", "biogeog", "Koeppen", "loaded.by")] # if there are still "NAC", and lat lon are not NA, that means the coordinates are falling in a place where no FAO are define... 
 
-if(any(is.na(SITES$Koeppen))) stop("There are missing Koeppen that you need to fill by hand in this script.")
+# plot(FAO)
+# points(SITES.xy[new.FAO %in% "NAC", ], pch = 2, col = "red")
+
+# SITES$FAO.ecozone[is.na(SITES$FAO.ecozone) & SITES$country %in% "Japan" & SITES$Koeppen %in% "Cfa"] <- "Subtropical humid forest"
+# SITES$FAO.ecozone[is.na(SITES$FAO.ecozone) & SITES$country %in% "United States of America" & SITES$Koeppen %in% c("Cfa", "Dfb")] <- "Temperate continental forest"
+# SITES$FAO.ecozone[is.na(SITES$FAO.ecozone) & SITES$country %in% c("Brazil", "French Guiana")  & SITES$Koeppen %in% "Af"] <- "Tropical rainforest"
+
+if(any(is.na(SITES$FAO.ecozone))) stop("There are missing Koeppen that you need to fill by hand in this script.")
 
 
 # Extract KOEPPEN ####
@@ -88,56 +99,62 @@ new.KOEPPEN$order <- 1:nrow(new.KOEPPEN)
 new.KOEPPEN <- merge(new.KOEPPEN, categoriesKOEPPEN, by = "GRIDCODE", all.x = T, sort = F)
 new.KOEPPEN <- new.KOEPPEN[order(new.KOEPPEN$order),]
 
-new.KOEPPEN[SITES$Koeppen %in% "NAC",] # if all Csb is ok, in our case
+new.KOEPPEN[SITES[idx.non.missing.coordinates, ]$Koeppen %in% "NAC",] # if all Csb is ok, in our case
 
 new.KOEPPEN <- new.KOEPPEN$Koeppen
 
-identical(SITES.xy$site.ID, SITES$site.ID)
+identical(SITES.xy$site.ID, SITES[idx.non.missing.coordinates, ]$site.ID)
 
-cbind(SITES$sites.sitename, SITES$Koeppen, new.KOEPPEN)[!apply(cbind(SITES$Koeppen, new.KOEPPEN), 1, function(x) x[1] %in% x[2]),] # should be empty
+cbind(SITES[idx.non.missing.coordinates, ]$sites.sitename, SITES[idx.non.missing.coordinates, ]$Koeppen, new.KOEPPEN)[!apply(cbind(SITES[idx.non.missing.coordinates, ]$Koeppen, new.KOEPPEN), 1, function(x) x[1] %in% x[2]),] # should be empty
 
-SITES[is.na(new.KOEPPEN), c("sites.sitename", "lat", "lon", "country", "Koeppen", "geography.notes", "climate.notes", "site.notes", "ref.notes", "site.ref")]
+SITES[idx.non.missing.coordinates, ][is.na(new.KOEPPEN), c("sites.sitename", "lat", "lon", "country", "Koeppen", "geography.notes", "climate.notes", "site.notes", "ref.notes", "site.ref", "loaded.by")]
 
 plot(KOEPPEN, col = factor(KOEPPEN$GRIDCODE))
 points(SITES.xy[is.na(new.KOEPPEN), ])
 
-# Replace all KOEPPEN by the ones extracted
 
-SITES$Koeppen <- new.KOEPPEN
+# Ignore NA and keep what was there before
+new.KOEPPEN[is.na(new.KOEPPEN)] <- SITES[idx.non.missing.coordinates, ]$Koeppen[is.na(new.KOEPPEN)]
+
+# Replace all KOEPPEN by the ones extracted
+SITES[idx.non.missing.coordinates, ]$Koeppen <- new.KOEPPEN
 
 SITES$Koeppen[is.na(SITES$Koeppen) & SITES$FAO.ecozone %in% "Tropical rainforest"] <- "Af"
+SITES$Koeppen[(SITES$Koeppen == "NAC") & SITES$FAO.ecozone %in% "Subtropical humid forest"] <- "Cfa"
 
 if(any(is.na(SITES$Koeppen))) stop("There are missing Koeppen that you need to fill by hand in this script.")
 
 # Extract biogeog ####
 plot(continents)
-points(SITES.xy, pch = 16, col = factor(new.biogeog))
 
 new.biogeog <- over(SITES.xy, continents)
 
-new.biogeog[SITES$biogeog %in% "NAC",]
+points(SITES.xy, pch = 16, col = factor(new.biogeog))
+
+new.biogeog[SITES[idx.non.missing.coordinates, ]$biogeog %in% "NAC",]
 
 new.biogeog <- new.biogeog$CONTINENT
 
 plot(SITES.xy, pch = 16, col = factor(SITES$biogeog)) # should look the same as previous plot
 
-identical(SITES.xy$site.ID, SITES$site.ID) # has to be TRUE
+identical(SITES.xy$site.ID, SITES[idx.non.missing.coordinates, ]$site.ID) # has to be TRUE
 
-cbind(SITES$sites.sitename, SITES$biogeog, new.biogeog)[!apply(cbind(SITES$biogeog, new.biogeog), 1, function(x) x[1] %in% x[2]),] # should be empty
+cbind(SITES[idx.non.missing.coordinates, ]$sites.sitename, SITES[idx.non.missing.coordinates, ]$biogeog, new.biogeog)[!apply(cbind(SITES[idx.non.missing.coordinates, ]$biogeog, new.biogeog), 1, function(x) x[1] %in% x[2]),] # should be empty
 
 
-SITES[is.na(new.biogeog), c("sites.sitename", "lat", "lon", "country")]
+SITES[idx.non.missing.coordinates, ][is.na(new.biogeog), c("sites.sitename", "lat", "lon", "country")]
 plot(continents)
 points(SITES.xy[is.na(new.biogeog),], pch = 2, col = "purple")
 
 # Replace all biogeog by the ones extracted + manually edit the ones that fall in the water
-SITES$biogeog <- new.biogeog
+SITES[idx.non.missing.coordinates, ]$biogeog <- new.biogeog
 
-SITES$biogeog[is.na(SITES$biogeog) & SITES$country %in% c("United States of America", "United States Virgin Islands")] <- "North America"
-SITES$biogeog[is.na(SITES$biogeog) & SITES$country %in% "Australia"] <- "Australia"
-SITES$biogeog[is.na(SITES$biogeog) & SITES$country %in% c("Malaysia", "Japan", "China", "Cambodia")] <- "Asia"
-SITES$biogeog[is.na(SITES$biogeog) & SITES$country %in% c("French Guiana", "Brazil")] <- "South America"
+SITES$biogeog[is.na(SITES$biogeog) & SITES$country %in% c("United States of America", "United States Virgin Islands", "USA")] <- "North America"
+SITES$biogeog[is.na(SITES$biogeog) & SITES$country %in% c("Australia", "New Zealand")] <- "Australia"
+SITES$biogeog[is.na(SITES$biogeog) & SITES$country %in% c("Malaysia", "Japan", "China", "Cambodia", "Turkey")] <- "Asia"
+SITES$biogeog[is.na(SITES$biogeog) & SITES$country %in% c("French Guiana", "Brazil", "Panama", "Belize")] <- "South America"
 
+SITES[is.na(SITES$biogeog),]
 
 if(any(is.na(SITES$biogeog))) stop("There are missing biogeog that you need to fill by hand in this script.")
 
