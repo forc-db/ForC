7323740ba45c88f3003f961006100da6a7f01aa0
Becky Banbury Morgan <becky.bm@hotmail.co.uk>
2021-04-16 13:15:59 +0100

    Update Extract_WorldClim_data.R

--- scripts/Database_manipulation/Extract_site_info_based_on_lat_long/Extract_WorldClim_data.R
+++ scripts/Database_manipulation/Extract_site_info_based_on_lat_long/Extract_WorldClim_data.R
@@ -18,8 +18,21 @@ proj<-CRS("+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0")
 proj4string(ForC_sites)<-proj
 plot(ForC_sites)
 
+# Download 30s res worldclim V 2.1
+setwd("C:/Users/gyrcbm/Documents/GitHub/ForC/supplementary_resources/WorldClim data/world_clim")
+P_url<- "https://biogeo.ucdavis.edu/data/worldclim/v2.1/base/wc2.1_30s_bio.zip"
+download.file(P_url, destfile="wc2.1_30s_bio.zip")
+unzip("wc2.1_30s_bio.zip")
+
+# Download 30s res worldclim V 2.1
+setwd("C:/Users/gyrcbm/Documents/GitHub/ForC/supplementary_resources/WorldClim data/world_clim")
+P_url<- "https://biogeo.ucdavis.edu/data/worldclim/v2.1/base/wc2.1_30s_tavg.zip"
+download.file(P_url, destfile="wc2.1_30s_tavg.zip")
+unzip("wc2.1_30s_tavg.zip")
+
+
 ### first extract MAT and MAP
-setwd("C:/Users/gyrcbm/Documents/GitHub/ForC/supplementary_resources/WorldClim data/world_clim/wc2.1_30s_bio/")
+setwd("C:/Users/gyrcbm/Documents/GitHub/ForC/supplementary_resources/WorldClim data/world_clim/")
 filenames<- c("wc2.1_30s_bio_1.tif","wc2.1_30s_bio_12.tif")
 files <- stack(filenames)
 
@@ -29,7 +42,7 @@ df_clim <- data.frame(siteID = df[,1], sites.sitename = as.character(df[,2]), si
 names(df_clim)[3:4] <- c("MAT", "MAP")
 
 ### then extract mean temperature of warmest and coldest months
-setwd("C:/Users/gyrcbm/Documents/GitHub/ForC/supplementary_resources/WorldClim data/world_clim/wc2.1_30s_tavg/")
+setwd("C:/Users/gyrcbm/Documents/GitHub/ForC/supplementary_resources/WorldClim data/world_clim/")
 MAT_filenames<- paste("wc2.1_30s_tavg_", c(paste(0,1:9, sep=""), 10, 11, 12), ".tif",sep="")
 MAT <- stack(MAT_filenames) 
 month <- c("01 Jan 2010", "01 Feb 2010", "01 Mar 2010", "01 Apr 2010", "01 May 2010", "01 Jun 2010", "01 Jul 2010", "01 Aug 2010", "01 Sep 2010", "01 Oct 2010", "01 Nov 2010", "01 Dec 2010")
